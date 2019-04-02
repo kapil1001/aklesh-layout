35dfe416377c256f62dabc84e61a3876e2827e95
kapil <pbhim67@gmail.com>
2019-04-02 19:20:25 +0545

    aklesh layout

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  
+    <script src="js/jquery-3.3.1.min.js"></script> 
+	<script src="js/bootstrap.min.js"></script>
+	
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" >
+	<link rel="stylesheet" type="text/css" href="assets/style.css">
+	<title>Cyber Security</title>
+</head>
+<body>
+	<div class="container-fluid top-bar">
+		<div class="col-sm-12">
+			<div class="row">
+				<div class="col-sm-8">
+					<p><i href="https://www.gmail.com" class="far fa-envelope"></i>info@softwarica.com
+					<i class="fas fa-phone"></i>+977-012345678</p>
+
+				</div>
+				
+				<div class="col-sm-4 d-flex flex-row-reverse">
+					<a href="https://www.facebook.com" class="social-icons"><i class="fab fa-facebook-square"></i></a>
+					<a href="https://www.google.com" class="social-icons"><i class="fab fa-google-plus-g"></i></a>
+					<a class="social-icons"  href="https://www.twitter.com">  <i  class="fab fa-twitter"></i></a>
+					<a 	href="https://www.youtube.com" class="social-icons"><i class="fab fa-youtube"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-4">
+				<img src="assets/images/softwarica.png" alt="logo of softwarica collage">
+			</div>
+			<div class="col-sm-8">
+				   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Softwarica College</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About us</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">BSC Hons</a>
+          <a class="dropdown-item" href="#">MBA</a>
+        
+          <a class="dropdown-item" href="#">BSC CSIT</a>
+        </div>
+
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">More</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+
+</nav>
+			</div>
+		</div>
+	</div>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/images/study.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/images/imag1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/images/library.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<h1>Who we are?</h1>
+<p> Softwarica College is working in collaboration with Coventry University to offer a range of undergraduate programmes. <br/>
+							On successful completion students will receive Coventry University degree award. <br/>
+							</p>
+
+<p> Coventry University is a forward-looking, pioneering, modern University.<br/>
+							 On successful completion students will receive Coventry University degree award.<br/>
+							</p>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      <img src="assets/images/business.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="col-sm">
+      <img src="assets/images/cyber.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="col-sm">
+      <img src="assets/images/programming.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="col-sm">
+      <img src="assets/images/designing.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
+
+<footer>
+	
+
+<div class="card text-center">
+  <div class="card-header" style="background-color:black;">
+    Join us at
+  </div>
+  <div class="card-body" style="background-color:black;">
+    <h5 class="card-title">&copy;softwarica </h5>
+    <!-- <p class="card-text">Best college in Town?</p> -->
+    <a href="#" class="btn btn-primary">Home</a>
+  </div>
+  <!-- <div class="card-footer text-muted">
+    2 days ago
+  </div> -->
+</div>
+</footer>
+</body>
+</html>
